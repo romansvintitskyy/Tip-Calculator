c3a6b7e6186578a2579cc6f37b1398d0d12515bd
Roman Svintitskyy <romansvintitskyy2023@u.northwestern.edu>
2020-09-14 13:09:03 +0300

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Roman Svintitskyy on 14.09.2020.
+//  Copyright © 2020 Roman Svintitskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
